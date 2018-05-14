f6f35180d36a4684f7f6c892fe96057e69871ab6
Ender <jaroslav.ender@gmail.com>
2018-05-14 21:51:57 +0700

    Added MangaController.List() and HomeController.About()

---
+++ OtakuNET.Web/Controllers/MangaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtakuNET.Web.Controllers
+{
+    public class MangaController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
