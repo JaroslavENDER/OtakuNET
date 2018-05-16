1a0129f46af9d547d7e0b51fcbb310ad73711b07
Ender <jaroslav.ender@gmail.com>
2018-05-16 14:16:11 +0700

    Added NewsController, ProfileController and StudioController, added AnimeController.Title() and .Season()

---
+++ OtakuNET.Web/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtakuNET.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Updates()
+        {
+            return View();
+        }
+    }
+}
---
+++ OtakuNET.Web/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtakuNET.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile(string login)
+        {
+            return View();
+        }
+
+        public IActionResult List(string loging, string name)
+        {
+            return View();
+        }
+    }
+}
