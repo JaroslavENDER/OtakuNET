c058bc3c6fd1d6a640eac72bca42e549f5616d9d
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-27 17:19:14 +0700

    Added IDbContext

---
+++ OtakuNET.Domain/DataProviders/IDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtakuNET.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OtakuNET.Domain.DataProviders
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<Animanga> Anime { get; set; }
+        DbSet<Animanga> Manga { get; set; }
+        DbSet<Season> Seasons { get; set; }
+        DbSet<Update> Updates { get; set; }
+        DbSet<News> News { get; set; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
