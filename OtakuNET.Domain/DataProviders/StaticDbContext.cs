232fdd9a0074ad4f4b192567d987531deb9fa6b8
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-27 18:37:53 +0700

    Added StaticDbContext

---
+++ OtakuNET.Domain/DataProviders/StaticDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtakuNET.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace OtakuNET.Domain.DataProviders
+{
+    public class StaticDbContext : DbContext, IDbContext
+    {
+        public DbSet<Anime> Anime { get; set; }
+        public DbSet<Manga> Manga { get; set; }
+        public DbSet<AnimeSeason> Seasons { get; set; }
+        public DbSet<Update> Updates { get; set; }
+        public DbSet<News> News { get; set; }
+
+        private static bool isInitialized = false;
+
+        public async Task<int> SaveChangesAsync()
+            => await base.SaveChangesAsync();
+
+        public StaticDbContext(DbContextOptions options)
+            : base(options)
+        {
+            if (isInitialized) return;
+            Initialize();
+            isInitialized = true;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => base.OnConfiguring(optionsBuilder);
+
+
+
+
+        private void Initialize()
+        {
+            Anime.Add(new Anime { Title = "First title" });
+            SaveChanges();
+        }
+    }
+}
