ca77f08a805b9db07349ba5705bdfc7377a39a27
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-28 15:33:11 +0700

    Fixed type of UserListInfoViewModel.Description

--- OtakuNET.Web/Models/ProfileViewModels/UserListInfoViewModel.cs
+++ OtakuNET.Web/Models/ProfileViewModels/UserListInfoViewModel.cs
@@ -4,6 +4,6 @@
     {
         public string Name { get; set; }
         public int TitleCount { get; set; }
-        public int Description { get; set; }
+        public string Description { get; set; }
     }
 }
