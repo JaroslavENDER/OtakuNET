71f5c39237c08f1bc0e91a28ffc3276ca61c92ce
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-29 00:38:23 +0700

    Added IndexViewModelExtensions

---
+++ OtakuNET.Web/ModelExtensions/HomeViewModelsExtentions/IndexViewModelExtensions.cs
@@ -0,0 +1,146 @@
+﻿using OtakuNET.Domain.Entities;
+using OtakuNET.Web.Models.AnimangaViewModels;
+using OtakuNET.Web.Models.HomeViewModels;
+using OtakuNET.Web.Models.NewsViewModels;
+using OtakuNET.Web.Models.ProfileViewModels;
+using OtakuNET.Web.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OtakuNET.Web.ModelExtensions.HomeViewModelsExtentions
+{
+    public static class IndexViewModelExtensions
+    {
+        public static IndexViewModel Initialize(this IndexViewModel model,
+            List<Anime> ongoings,
+            List<object> userAnimeLists,
+            List<object> userMangaList,
+            List<AnimeSeason> lastSeasons,
+            List<News> lastNews,
+            List<Update> lastUpdates,
+            ITimestampFormatter timestampFormatter,
+            ITagTranslator tagTranslator)
+        {
+            model.Ongoings = ongoings
+                .OrderByDescending(a => a.Updates.Max(u => u.Timestamp))
+                .Take(8)
+                .Select(a => new TitlePreviewViewModel
+                {
+                    Name = a.Title,
+                    Info = a.StudioName,
+                    ImageSrc = a.ImageSrc
+                })
+                .ToList();
+
+            model.UserAnimeLists = userAnimeLists?.Count > 0
+                ? model.UserAnimeLists = userAnimeLists.Select(o => new UserListInfoViewModel()).ToList()
+                : new List<UserListInfoViewModel>
+                {
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Запланировано", Description = 0 },
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Смотрю", Description = 0 },
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Пересматриваю", Description = 0 },
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Просмотрено", Description = 0 },
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Отложено", Description = 0 },
+                    new UserListInfoViewModel{ TitleCount = 0, Name = "Брошено", Description = 0 }
+                };
+
+            model.UserMangaLists = userMangaList?.Count > 0
+                ? userMangaList.Select(o => new UserListInfoViewModel()).ToList()
+                : new List<UserListInfoViewModel>
+                {
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Запланировано", Description = 0 },
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Читаю", Description = 0 },
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Перечитываю", Description = 0 },
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Прочитано", Description = 0 },
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Отложено", Description = 0 },
+                        new UserListInfoViewModel{ TitleCount = 0, Name = "Брошено", Description = 0 }
+                };
+
+            model.AnimeRecomendationsFirstBlock = lastSeasons.Select(n => new RecomendationInfoViewModel { Text = n.Name, Link = n.Key, LinkTitle = n.FullName }).ToList();
+            model.AnimeRecomendationsSecondBlock = new List<RecomendationInfoViewModel>
+            {
+                new RecomendationInfoViewModel
+                {
+                    Text = "Избранное",
+                    Link = "",
+                    LinkTitle = "Избранное"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "От сообщества",
+                    Link = "",
+                    LinkTitle = "От сообщества"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "Персонализированные",
+                    Link = "",
+                    LinkTitle = "Персонализированные"
+                }
+            };
+            model.MangaRecomendationsFirstBlock = new List<RecomendationInfoViewModel>
+            {
+                new RecomendationInfoViewModel
+                {
+                    Text = "Манга",
+                    Link = "",
+                    LinkTitle = "Манга"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "Ваншот",
+                    Link = "",
+                    LinkTitle = "Ваншот"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "Додзинси",
+                    Link = "",
+                    LinkTitle = "Додзинси"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "Маньхуа",
+                    Link = "",
+                    LinkTitle = "Маньхуа"
+                }
+            };
+            model.MangaRecomendationsSecondBlock = new List<RecomendationInfoViewModel>
+            {
+                new RecomendationInfoViewModel
+                {
+                    Text = "Избранное",
+                    Link = "",
+                    LinkTitle = "Избранное"
+                },
+                new RecomendationInfoViewModel
+                {
+                    Text = "Персонализированные",
+                    Link = "",
+                    LinkTitle = "Персонализированные"
+                }
+            };
+
+            model.News = lastNews
+                .Select(n => new OneNewsViewModel
+                {
+                    Title = n.Title,
+                    Timestamp = timestampFormatter.Format(n.Timestamp),
+                    ImageSrc = n.ImageSrc,
+                    Text = n.Text
+                })
+                .ToList();
+            model.Updates = lastUpdates
+                .Select(u => new OneUpdateViewModel
+                {
+                    Title = u.Anime.Title,
+                    Tag = tagTranslator.ToTag(u.Tag),
+                    Timestamp = timestampFormatter.Format(u.Timestamp),
+                    ImageSrc = u.Anime.ImageSrc
+                })
+                .ToList();
+
+            return model;
+        }
+    }
+}
---
+++ tests/OtakuNET.Web.Tests/ModelExtentions/HomeViewModelsExtentions/IndexViewModelExtentionsTest.cs
@@ -0,0 +1,148 @@
+﻿using Moq;
+using OtakuNET.Domain.Entities;
+using OtakuNET.Web.ModelExtensions.HomeViewModelsExtentions;
+using OtakuNET.Web.Models;
+using OtakuNET.Web.Models.HomeViewModels;
+using OtakuNET.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace OtakuNET.Web.Tests.ModelExtentions.HomeViewModelsExtentions
+{
+    public class IndexViewModelExtentionsTest
+    {
+        [Fact]
+        public void Returns8OngoingsWithLatestUpdateTimestamp()
+        {
+            var ongoings = new List<Anime>
+            {
+                new Anime
+                {
+                    Title = "Мегалобокс",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddMonths(-1),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Семь смертных грехов",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddDays(-25),
+                        }
+                    }
+                },
+                new Anime
+            {
+                Title = "Трусливый велосипедист",
+                Updates = new List<Update>
+                {
+                    new Update
+                    {
+                        Timestamp = DateTime.Now.AddDays(-8),
+                    }
+                }
+            },
+                new Anime
+                {
+                    Title = "Хладнокровный Казуки",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddDays(-7),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Старшая школа DxD",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddDays(-6),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Второй Мейджор",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddDays(-1),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Хисонэ и Масо",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddHours(-2),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Персона 5",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now.AddMinutes(-30),
+                        }
+                    }
+                },
+                new Anime
+                {
+                    Title = "Повар-боец Сома: Третье блюдо - Часть II",
+                    Updates = new List<Update>
+                    {
+                        new Update
+                        {
+                            Timestamp = DateTime.Now,
+                        }
+                    }
+                }
+            };
+            var timestampFormatterMock = new Mock<ITimestampFormatter>();
+            timestampFormatterMock.Setup(m => m.Format(It.IsAny<DateTime>())).Returns<DateTime>(dt => dt.ToLongDateString());
+            var tagTranslatorMock = new Mock<ITagTranslator>();
+            tagTranslatorMock.Setup(m => m.ToTag(It.IsAny<string>())).Returns<string>(tag => Tag.Release);
+
+            var result = new IndexViewModel().Initialize(ongoings, null, null, new List<AnimeSeason>(), new List<News>(), new List<Update>(), timestampFormatterMock.Object, tagTranslatorMock.Object);
+
+            Assert.Equal(8, result.Ongoings.Count);
+            Assert.Null(result.Ongoings.FirstOrDefault(a => a.Name == "Мегалобокс"));
+        }
+
+        [Fact]
+        public void ReturnsDefaultUserListsFromNulls()
+        {
+            var timestampFormatterMock = new Mock<ITimestampFormatter>();
+            timestampFormatterMock.Setup(m => m.Format(It.IsAny<DateTime>())).Returns<DateTime>(dt => dt.ToLongDateString());
+            var tagTranslatorMock = new Mock<ITagTranslator>();
+            tagTranslatorMock.Setup(m => m.ToTag(It.IsAny<string>())).Returns<string>(tag => Tag.Release);
+
+            var result = new IndexViewModel().Initialize(new List<Anime>(), null, null, new List<AnimeSeason>(), new List<News>(), new List<Update>(), timestampFormatterMock.Object, tagTranslatorMock.Object);
+
+            Assert.Equal(6, result.UserAnimeLists.Count);
+            Assert.Equal(6, result.UserMangaLists.Count);
+            Assert.Equal("Запланировано", result.UserMangaLists.First().Name);
+            Assert.Equal("Просмотрено", result.UserAnimeLists[3].Name);
+        }
+    }
+}
