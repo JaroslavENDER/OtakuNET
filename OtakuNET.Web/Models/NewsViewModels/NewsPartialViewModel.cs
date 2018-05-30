c3ffa7d7e483d3399c6fca47b52795061bc8690b
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-30 11:43:03 +0700

    Added Views/NewsPartial/ and NewsViewModels/

---
+++ OtakuNET.Web/Models/NewsViewModels/NewsPartialViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OtakuNET.Web.Models.NewsViewModels
+{
+    public class NewsPartialViewModel
+    {
+        public List<OneNewsViewModel> News { get; set; }
+    }
+}
---
+++ OtakuNET.Web/Models/NewsViewModels/UpdatesPartialViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OtakuNET.Web.Models.NewsViewModels
+{
+    public class UpdatesPartialViewModel
+    {
+        public List<OneUpdateViewModel> Updates { get; set; }
+    }
+}
