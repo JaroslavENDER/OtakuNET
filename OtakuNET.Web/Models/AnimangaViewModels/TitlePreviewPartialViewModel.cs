91e79ba42e86bc9cf3618095e7f3f8188b09909c
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-30 11:46:03 +0700

    Added TitlePreviewPartial and TitlePreviewPartialViewModel

---
+++ OtakuNET.Web/Models/AnimangaViewModels/TitlePreviewPartialViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OtakuNET.Web.Models.AnimangaViewModels
+{
+    public class TitlePreviewPartialViewModel
+    {
+        public TitlePreviewViewModel Title { get; set; }
+        public string TitlePreviewBlockId { get; set; }
+        public string ControllerName { get; set; }
+    }
+}
