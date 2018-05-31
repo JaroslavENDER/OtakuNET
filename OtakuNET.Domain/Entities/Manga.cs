9f7a586bc283fc832e6490c26978b2ccb80ca86c
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-31 20:35:49 +0700

    Added Manga.Type

--- OtakuNET.Domain/Entities/Manga.cs
+++ OtakuNET.Domain/Entities/Manga.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OtakuNET.Domain.Entities
 {
     public class Manga : Animanga
     {
+        [Required] public string Type { get; set; }
+
         public List<Manga_MangaList> UserLists { get; set; }
 
         public Manga()
