cbff3cc7b77809a449a27925a56e9c26e071112a
Ender <pavlenko.jaroslav@yandex.ru>
2018-06-08 13:26:18 +0700

    Added test for ProfileCreaterService

---
+++ tests/OtakuNET.Web.Tests/Services/ProfileCreater/ProfileCreaterTest.cs
@@ -0,0 +1,23 @@
+﻿using OtakuNET.Web.Services.ProfileCreater;
+using Xunit;
+
+namespace OtakuNET.Web.Tests.Services
+{
+    public class ProfileCreaterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var id = "qwe";
+            var login = "testLogin";
+
+            var result = new ProfileCreater().Create(id, login);
+
+            Assert.Equal(id, result.Id);
+            Assert.Equal(login, result.Login);
+            Assert.Equal(6, result.AnimeListSet.Count);
+            Assert.Equal(6, result.MangaListSet.Count);
+            Assert.Single(result.History);
+        }
+    }
+}
