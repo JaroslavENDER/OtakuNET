4e3d1ab1d1066fe17548050a883311aab1fd1992
Ender <pavlenko.jaroslav@yandex.ru>
2018-06-09 16:09:02 +0700

    Added comments viewModels and extensions

---
+++ OtakuNET.Web/ModelExtensions/CommentsViewModelsExtensions/CommentsComponentViewModelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Ender.TimestampFormatterCore;
+using OtakuNET.Domain.Entities;
+using OtakuNET.Web.Models.CommentsViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OtakuNET.Web.ModelExtensions.CommentsViewModelsExtensions
+{
+    public static class CommentsComponentViewModelExtensions
+    {
+        public static CommentsComponentViewModel Initialize(this CommentsComponentViewModel model, IEnumerable<Comment> comments, bool isUserSignIn, ITimestampFormatter timestampFormatter)
+        {
+            model.IsUserSignIn = isUserSignIn;
+            model.Comments = comments.Select(c => new CommentViewModel().Initialize(c, timestampFormatter)).ToList();
+
+            return model;
+        }
+    }
+}
---
+++ OtakuNET.Web/Models/CommentsViewModels/CommentsComponentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OtakuNET.Web.Models.CommentsViewModels
+{
+    public class CommentsComponentViewModel
+    {
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+        public bool IsUserSignIn { get; set; }
+    }
+}
