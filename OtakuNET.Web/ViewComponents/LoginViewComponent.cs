0afc80eb78055b6021a3c29d1388f97a82c4dcb0
Ender <pavlenko.jaroslav@yandex.ru>
2018-06-11 12:58:24 +0700

    Replaced _LoginPartial on LoginViewComponent

---
+++ OtakuNET.Web/ViewComponents/LoginViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OtakuNET.Web.Models;
+using System.Threading.Tasks;
+
+namespace OtakuNET.Web.ViewComponents
+{
+    public class LoginViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        public LoginViewComponent(UserManager<ApplicationUser> userManager)
+            => this.userManager = userManager;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await userManager.GetUserAsync(HttpContext.User);
+            return View(user?.UserName as object);
+        }
+    }
+}
